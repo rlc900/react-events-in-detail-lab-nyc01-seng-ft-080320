8b6d094ede1968f7f6dd54fdd6b31edc5e2c0a4a
Rianna Cleary <rianna.cleary@flatironschool.com>
2020-10-08 14:59:14 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,14 @@
 // Code DelayedButton Component Here
+import React, { Component } from 'react';
+
+class DelayedButton extends Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default DelayedButton;
