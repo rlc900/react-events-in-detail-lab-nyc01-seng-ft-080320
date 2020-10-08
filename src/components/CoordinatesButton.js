0b38a6ca525ef84e4db3e8838c12da59aaf9016c
Rianna Cleary <rianna.cleary@flatironschool.com>
2020-10-08 14:56:15 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,14 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+class CoordinatesButton extends Component {
+
+  render() {
+    return (
+      <div></div>
+    );
+  }
+
+}
+
+export default CoordinatesButton;
